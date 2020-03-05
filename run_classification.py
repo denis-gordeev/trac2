84960cdf8748604a5f5582fa9945adbd263ee62f
Denis <gordeev-d-i@yandex.ru>
2020-03-05 19:18:42 +0300

    another exception

--- run_classification.py
+++ run_classification.py
@@ -503,43 +503,46 @@ def evaluate(args, model, tokenizer, prefix=""):
         preds = None
         out_label_ids = None
         for batch in tqdm(eval_dataloader, desc="Evaluating"):
-            model.eval()
-            batch = tuple(t.to(args.device) for t in batch)
+            try:
+                model.eval()
+                batch = tuple(t.to(args.device) for t in batch)
 
-            with torch.no_grad():
-                inputs = {
-                    "input_ids": batch[0],
-                    "attention_mask": batch[1],
-                    "labels_a": batch[3],
-                    "labels_b": batch[4],
-                }
-                if args.model_type != "distilbert":
-                    inputs["token_type_ids"] = (
-                        batch[2]
-                        if args.model_type in ["bert", "xlnet", "albert"]
-                        else None
-                    )  # XLM, DistilBERT, RoBERTa, and XLM-RoBERTa don't use segment_ids
-                outputs = model(**inputs)
-                tmp_eval_loss, logits = outputs[:2]
+                with torch.no_grad():
+                    inputs = {
+                        "input_ids": batch[0],
+                        "attention_mask": batch[1],
+                        "labels_a": batch[3],
+                        "labels_b": batch[4],
+                    }
+                    if args.model_type != "distilbert":
+                        inputs["token_type_ids"] = (
+                            batch[2]
+                            if args.model_type in ["bert", "xlnet", "albert"]
+                            else None
+                        )  # XLM, DistilBERT, RoBERTa, and XLM-RoBERTa don't use segment_ids
+                    outputs = model(**inputs)
+                    tmp_eval_loss, logits = outputs[:2]
 
-                eval_loss += tmp_eval_loss.mean().item()
-            nb_eval_steps += 1
-            if preds is None:
-                preds = logits.detach().cpu().numpy()
-                out_label_ids_a = inputs["labels_a"].detach().cpu().numpy()
-                out_label_ids_b = inputs["labels_b"].detach().cpu().numpy()
-            else:
-                preds = np.append(preds, logits.detach().cpu().numpy(), axis=0)
-                out_label_ids_a = np.append(
-                    out_label_ids,
-                    inputs["labels_a"].detach().cpu().numpy(),
-                    axis=0,
-                )
-                out_label_ids_b = np.append(
-                    out_label_ids,
-                    inputs["labels_b"].detach().cpu().numpy(),
-                    axis=0,
-                )
+                    eval_loss += tmp_eval_loss.mean().item()
+                nb_eval_steps += 1
+                if preds is None:
+                    preds = logits.detach().cpu().numpy()
+                    out_label_ids_a = inputs["labels_a"].detach().cpu().numpy()
+                    out_label_ids_b = inputs["labels_b"].detach().cpu().numpy()
+                else:
+                    preds = np.append(preds, logits.detach().cpu().numpy(), axis=0)
+                    out_label_ids_a = np.append(
+                        out_label_ids_a,
+                        inputs["labels_a"].detach().cpu().numpy(),
+                        axis=0,
+                    )
+                    out_label_ids_b = np.append(
+                        out_label_ids_b,
+                        inputs["labels_b"].detach().cpu().numpy(),
+                        axis=0,
+                    )
+            except Exception as ex:
+                print(ex, "evaluate")
         try:
             eval_loss = eval_loss / nb_eval_steps
             if args.output_mode == "classification":
