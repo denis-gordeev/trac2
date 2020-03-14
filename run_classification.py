664f9e741ecf74800c7bb59bb29e86cde5024bb6
Denis Gordeev <dnsgrdv@gmail.com>
2020-03-14 13:30:34 +0300

    fixed letters

--- run_classification.py
+++ run_classification.py
@@ -620,7 +620,7 @@ def evaluate(args, model, tokenizer, prefix=""):
                     for key in sorted(result_b.keys()):
                         logger.info("  %s = %s", key, str(result_b[key]))
                         writer.write("%s = %s\n" % (key, str(result_b[key])))
-            for letter, preds in (("_a", preds_a), ("_a", preds_a)):
+            for letter, preds in (("_a", preds_a), ("_b", preds_b)):
                 output_test_predictions_file = os.path.join(
                     args.output_dir, "test_predictions" +
                     "_".join(args.folder_list) +
